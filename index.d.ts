b4ecff1f817c89de896e65c1664a304794f1d8eb
Serge <dasilvasoa@gmail.com>
2019-02-03 16:40:40 +0100

    Ignore .d.ts

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './amp';
-export * from './user';
-export * from './validation';
-export * from './nowplaying';
--- validation.d.ts
+++
@@ -1,8 +0,0 @@
-export interface IValidationMessage extends Error {
-    message: string;
-    name: string;
-    type?: string;
-    params?: {
-        [index: string]: any;
-    };
-}
