2b7a28daccfa491a4361fe705b1a94f3f132280a
Serge <vapkse@gmail.com>
2021-12-31 21:19:20 +0100

    fix(): replace musicstory with theaudiodb

---
+++ now-playing.ts
@@ -0,0 +1,60 @@
+
+export interface NowPlayingResponse {
+    artists: ReadonlyArray<string>;
+    album: string;
+    title: string;
+    artistsInfos: Array<Artist>;
+}
+
+export interface Artist {
+    idArtist: string;
+    idLabel: string;
+    intBornYear: string;
+    intCharted: string;
+    intDiedYear: string;
+    intFormedYear: string;
+    intMembers: string;
+    strArtist: string;
+    strArtistAlternate: string;
+    strArtistBanner: string;
+    strArtistClearart: string;
+    strArtistFanart: string;
+    strArtistFanart2: string;
+    strArtistFanart3: string;
+    strArtistFanart4: string;
+    strArtistLogo: string;
+    strArtistStripped: string;
+    strArtistThumb: string;
+    strArtistWideThumb: string;
+    strBiography: string;
+    strBiographyCN: string;
+    strBiographyDE: string;
+    strBiographyES: string;
+    strBiographyFR: string;
+    strBiographyHU: string;
+    strBiographyIL: string;
+    strBiographyIT: string;
+    strBiographyJP: string;
+    strBiographyNL: string;
+    strBiographyNO: string;
+    strBiographyPL: string;
+    strBiographyPT: string;
+    strBiographyRU: string;
+    strBiographySE: string;
+    strCountry: string;
+    strCountryCode: string;
+    strDisbanded: string;
+    strFacebook: string;
+    strGender: string;
+    strGenre: string;
+    strISNIcode: string;
+    strLabel: string;
+    strLastFMChart: string;
+    strLocked: string;
+    strMood: string;
+    strMusicBrainzID: string;
+    strStyle: string;
+    strTwitter: string;
+    strWebsite: string;
+    error: Error;
+}
